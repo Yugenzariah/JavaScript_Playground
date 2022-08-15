a3a47e767438704e72a15957f5769dc5c69df6af
Yugenzariah <KeithJhaeron-Cayatoc@live.nmit.ac.nz>
2022-08-16 11:19:52 +1200

    First Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Create a function that takes an integer(minutes) and convert it to seconds. 
+
+function convertToSeconds(minutes) {
+    return minutes * 60;
+}
+
+console.log(convertToSeconds(5));
